5f5149377dfcc2efcdc950079102589a14a09a96
AK3727 <willsonj94@gmail.com>
2018-06-19 15:19:26 +0530

    Add files via upload

---
+++ ExceptionDemo.py
@@ -0,0 +1,10 @@
+#Exception is some sort of code which cause your program to terminate
+def div(a, b):
+    return a/b
+
+
+print(div(3, 0))
+
+#these Type of things are called exceptions; what exceptions do is that it doesn't allow your code to run further
+
+print('hello')
